6df2f24246277ab3555da02de6bbfcbe7d1f2447
John Doe <johndoe@example.com>
2024-05-28 15:26:58 +0800

    1

--- src/Session.php
+++ src/Session.php
@@ -10,59 +10,35 @@ class Session
 
     public function __construct(string $prefix = null)
     {
-        if (is_null($prefix)) {
-            $prefix = dirname('/' . implode('/', array_filter(
-                explode('/', $_SERVER['SCRIPT_NAME']),
-                function ($val) {
-                    return strlen($val) > 0 ? true : false;
-                }
-            )));
-        }
-        $this->prefix = $prefix;
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
+
+        $this->prefix = is_null($prefix) ? dirname('/' . implode('/', array_filter(
+            explode('/', $_SERVER['SCRIPT_NAME']),
+            function ($val) {
+                return strlen($val) > 0 ? true : false;
+            }
+        ))) : $prefix;
     }
 
-    public function set(string $name, $value): self
+    public function set(string $name, $value)
     {
-        $_SESSION[$this->prefix . $name] = $value;
-        return $this;
+        $_SESSION[$this->prefix . $name] = serialize($value);
     }
 
     public function get(string $name, $default = null)
     {
-        return isset($_SESSION[$this->prefix . $name]) ? $_SESSION[$this->prefix . $name] : $default;
+        return isset($_SESSION[$this->prefix . $name]) ? unserialize($_SESSION[$this->prefix . $name]) : $default;
     }
 
-    public function delete(string $name): self
+    public function delete(string $name)
     {
         unset($_SESSION[$this->prefix . $name]);
-        return $this;
     }
 
     public function has(string $name): bool
     {
         return isset($_SESSION[$this->prefix . $name]);
-    }
-
-    public function __set($name, $value)
-    {
-        $this->set($name, $value);
-    }
-
-    public function __get($name)
-    {
-        return $this->get($name);
-    }
-
-    public function __unset($name)
-    {
-        $this->delete($name);
-    }
-
-    public function __isset($name)
-    {
-        return $this->has($name);
     }
 }
